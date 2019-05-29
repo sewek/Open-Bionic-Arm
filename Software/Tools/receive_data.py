36f9ea2a9a48336c90536d75acba79197697094d
sewek <seweryn.malczewski@gmail.com>
2019-05-29 10:06:41 +0200

    Update receive_data.py

--- Software/Tools/receive_data.py
+++ Software/Tools/receive_data.py
@@ -1,25 +1,10 @@
 import serial
 import serial.tools.list_ports
+import threading
+import logging
 import time
 from dataclasses import dataclass
 
-print("Find out ports:")
-ports = serial.tools.list_ports.comports()
-for port in ports:
-    print("- " + port.device)
-
-print("Select port: ", end = '')
-port = input()
-
-serial_port = serial.Serial(port, 115200)
-serial_port.flushInput()
-serial_port.flushOutput()
-print("Connected with: " + serial_port.name)
-
-# Send signal to start giving data 0xa0 = 160
-serial_port.write(0xa0)
-while serial_port.read() != 0x01: # 0x01 means everything is OK!
-    time.sleep(1)
 
 # data protocol structure
 #    [byte]          2x[byte]        2x[byte]
@@ -31,11 +16,43 @@ class protocol:
     amplitude    : int
     deltatime    : int
 
+def make_serial():
+    print("Find out ports:")
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        print("- " + port.device)
+    print("Select port: ", end = '')
+    port = input()
+    serial_port = serial.Serial(port, 115200)
+    serial_port.flushInput()
+    serial_port.flushOutput()
+    print("Connected with: " + serial_port.name)
+        
+def read_signal_thread_func():
+    print("Start of read thread...")
+    i = 0
+    while can_read_signal:
+        data[i].deltatime = serial_port.read()
+        data[i].amplitude = serial_port.read(2)
+        data[i].deltatime = serial_port.read(2)
+        i+=1
+    print("Stop of read thread...")
+
+can_read_signal = False
 data :protocol = []
 
-i = 0
-while True:
-    data[i].deltatime = serial_port.read()
-    data[i].amplitude = serial_port.read(2)
-    data[i].deltatime = serial_port.read(2)
-    i+=1
+make_serial()
+
+read_signal_thread = threading.Thread(target=read_signal_thread_func)
+
+print("Start data read ?(y/n)")
+res = input()
+if res == "y":
+    # Send signal to start giving data 0xa0 = 160
+    serial_port.write(0xa0)
+    while serial_port.read() != 0x01: # 0x01 means everything is OK!
+        time.sleep(1)
+    read_signal_thread.start()
+else:
+    quit()
+
