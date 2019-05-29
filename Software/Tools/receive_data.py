34d91ed306a3a396289eda4ac6977c70a8b5e69c
sewek <seweryn.malczewski@gmail.com>
2019-05-29 19:39:34 +0200

    Update receive_data.py

--- Software/Tools/receive_data.py
+++ Software/Tools/receive_data.py
@@ -5,6 +5,11 @@ import logging
 import time
 from dataclasses import dataclass
 
+# commands
+# 0xa0 - OK
+# 0xa1 - ERROR
+# 0xaa - start sending data
+# 0xab - stop sending data
 
 # data protocol structure
 #    [byte]          2x[byte]        2x[byte]
@@ -17,8 +22,12 @@ class protocol:
     deltatime    : int
 
 def make_serial():
-    print("Find out ports:")
     ports = serial.tools.list_ports.comports()
+    if len(ports) < 1:
+        print("There are not connected devices!")
+        time.sleep(3)
+        quit()
+    print("Find out ports:")
     for port in ports:
         print("- " + port.device)
     print("Select port: ", end = '')
@@ -28,18 +37,81 @@ def make_serial():
     serial_port.flushOutput()
     print("Connected with: " + serial_port.name)
         
+def make_menu(command):
+    if command == "q":
+        can_read_signal = False
+        keep_read_thread_alive = False
+        serial_port.write(0xab)
+        while serial_port.read() != 0xa0:
+            time.sleep(1)
+        read_signal_thread.join() # Let's finish thread
+        serial_port.close()
+        print("Good bye!")
+        quit()
+    elif command == "s":
+        can_read_signal = False
+        serial_port.write(0xab)
+        while serial_port.read() != 0xa0:
+            time.sleep(1)
+        print("Stopped.")
+    elif command == "p":
+        serial_port.flushInput()
+        serial_port.flushOutput()
+        serial_port.write(0xaa)
+        while serial_port.read() != 0xa0:
+            time.sleep(1)
+        can_read_signal = True
+        print("Played.")
+    elif command == "c":
+        clear_data = True
+        can_read_signal = False
+        while clear_data:
+            time.sleep(1) # do nothing
+        can_read_signal = True
+        print("Data cleared.")
+    elif command == "w":
+        # Save data, and show graph
+        can_read_signal = False
+        file = open("data.csv", "w")
+        file.write("Id,Amplitude,Deltatime")
+        i = 0
+        for i in range(len(data)-1):
+            file.write(data[i].electrode_id + "," + data[i].amplitude + "," + data[i].deltatime)
+        file.close()
+        print("Data saved!")
+    elif command == "m":
+        print("***** HELP MENU *****")
+        print("s - to stop receiving data")
+        print("p - to play receiving data")
+        print("c - to clear received data")
+        print("w - to save all data, and show graph")
+        print("m - to show this menu")
+        print("q - to quit from program")
+    print()
+    print(": ")
+
 def read_signal_thread_func():
     print("Start of read thread...")
     i = 0
-    while can_read_signal:
-        data[i].deltatime = serial_port.read()
-        data[i].amplitude = serial_port.read(2)
-        data[i].deltatime = serial_port.read(2)
-        i+=1
+    while keep_read_thread_alive:
+        if can_read_signal == False:
+            time.sleep(1)
+            continue
+        if clear_data:
+            i = 0
+            clear_data = False
+        while can_read_signal:
+            data[i].deltatime = serial_port.read()
+            data[i].amplitude = serial_port.read(2)
+            data[i].deltatime = serial_port.read(2)
+            i+=1
     print("Stop of read thread...")
 
+keep_read_thread_alive = True
 can_read_signal = False
+clear_data = False
 data :protocol = []
+serial_port = None
 
 make_serial()
 
@@ -47,12 +119,18 @@ read_signal_thread = threading.Thread(target=read_signal_thread_func)
 
 print("Start data read ?(y/n)")
 res = input()
-if res == "y":
-    # Send signal to start giving data 0xa0 = 160
-    serial_port.write(0xa0)
-    while serial_port.read() != 0x01: # 0x01 means everything is OK!
-        time.sleep(1)
-    read_signal_thread.start()
-else:
+if res != "y":
     quit()
 
+# Send signal to start giving data 0xaa
+serial_port.write(0xaa)
+while serial_port.read() != 0xa0: # 0xa0 means everything is OK!
+    time.sleep(1)
+
+print("Passed init.")
+read_signal_thread.start()
+can_read_signal = True
+
+print("(m): ")
+res = input()
+make_menu(res)
